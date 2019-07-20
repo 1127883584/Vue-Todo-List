7a2c2b0db51781f8cc3b49d24b9e61ed9fec85e1
1127883584@qq.com <1127883584@qq.com>
2019-07-20 12:15:58 +0800

    [eric.li][impliement: show todo list]

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app" style="background-color:#eee;position: fixed;width: 100%;height: 100%;">
+    <List/>
+  </div>
+</template>
+
+<script>
+import List from './components/List.vue'
+export default {
+  name: 'app',
+  components: {
+    List
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
